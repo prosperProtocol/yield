1487e467f22c07a6868decbbaed03eb40e4724b2
josean7link <josean7link@gmail.com>
2026-06-03 21:24:07 -0400

    (feat) memo

--- contracts/yield/src/lib.rs
+++ contracts/yield/src/lib.rs
@@ -1,6 +1,6 @@
 #![no_std]
 use soroban_sdk::{contract, contractimpl, contracttype, contracterror, panic_with_error};
-use soroban_sdk::{Env, Address};
+use soroban_sdk::{Env, Address, Vec};
 use soroban_sdk::{IntoVal, symbol_short};
 
 const ADMIN_KEY: &str = "ADMIN";
@@ -19,6 +19,7 @@ pub enum StrategyStatus {
 #[derive(Clone, Debug, PartialEq, Eq)]
 pub struct Strategy {
     pub user: Address,
+    pub memo: u64,
     pub amount: i128,
     pub pct: i128,
     pub token: Address,
@@ -136,6 +137,7 @@ impl YieldDistributor {
     pub fn set_strat(
         env: Env,
         user: Address,
+        memo: u64,
         amount: i128
     ) {
         let admin = Self::get_admin(&env);
@@ -147,6 +149,7 @@ impl YieldDistributor {
 
         let rule = Strategy {
             user: user.clone(),
+            memo,
             amount,
             pct,
             token,
@@ -155,20 +158,46 @@ impl YieldDistributor {
             status: StrategyStatus::Active,
         };
 
-        env.storage().instance().set(&user, &rule);
+        let mut strats: Vec<Strategy> = env.storage().instance().get(&user).unwrap_or(Vec::new(&env));
+        strats.push_back(rule);
+        env.storage().instance().set(&user, &strats);
     }
 
     // funcion para obtener la estrategia de un usuario get_strategy o get_strat
-    pub fn get_strat(env: &Env, user: Address) -> Strategy {
-        env.storage().instance().get(&user)
-            .unwrap_or_else(|| panic_with_error!(env, YieldError::NotInitialized))
+    pub fn get_strat(env: &Env, user: Address, memo: u64) -> Strategy {
+        let strats: Vec<Strategy> = env.storage().instance().get(&user)
+            .unwrap_or_else(|| panic_with_error!(env, YieldError::NotInitialized));
+        for strat in strats.iter() {
+            if strat.memo == memo {
+                return strat;
+            }
+        }
+        panic_with_error!(env, YieldError::InvalidStrategy)
+    }
+
+    pub fn get_all_strats(env: &Env, user: Address) -> Vec<Strategy> {
+        env.storage().instance().get(&user).unwrap_or(Vec::new(env))
+    }
+
+    fn update_strat(env: &Env, user: Address, updated_strat: Strategy) {
+        let strats: Vec<Strategy> = env.storage().instance().get(&user)
+            .unwrap_or_else(|| panic_with_error!(env, YieldError::NotInitialized));
+        let mut new_strats = Vec::new(env);
+        for strat in strats.iter() {
+            if strat.memo == updated_strat.memo {
+                new_strats.push_back(updated_strat.clone());
+            } else {
+                new_strats.push_back(strat);
+            }
+        }
+        env.storage().instance().set(&user, &new_strats);
     }
 
     // funcion para cambiar el estado de la estrategia a Expired
-    pub fn set_s_exp(env: Env, user: Address) {
+    pub fn set_s_exp(env: Env, user: Address, memo: u64) {
         let admin = Self::get_admin(&env);
         admin.require_auth();
-        let mut strategy = Self::get_strat(&env, user.clone());
+        let mut strategy = Self::get_strat(&env, user.clone(), memo);
         if strategy.status != StrategyStatus::Active {
             panic_with_error!(&env, YieldError::InvalidStatus);
         }
@@ -178,59 +207,60 @@ impl YieldDistributor {
         let token_client: CustomTokenClient<'_> = CustomTokenClient::new(&env, &strategy.token);
         token_client.mint(&user, &yield_amount, &admin);
         strategy.status = StrategyStatus::Expired;
-        env.storage().instance().set(&user, &strategy);
+        Self::update_strat(&env, user.clone(), strategy);
     }
 
     // funcion para cambiar el estado de la estrategia a Completed
-    pub fn set_s_cmp(env: Env, user: Address) {
+    pub fn set_s_cmp(env: Env, user: Address, memo: u64) {
         let admin = Self::get_admin(&env);
         admin.require_auth();
-        let mut strategy = Self::get_strat(&env, user.clone());
+        let mut strategy = Self::get_strat(&env, user.clone(), memo);
         if strategy.status != StrategyStatus::Expired {
             panic_with_error!(&env, YieldError::InvalidStatus);
         }
         strategy.status = StrategyStatus::Completed;
-        env.storage().instance().set(&user, &strategy);
+        Self::update_strat(&env, user.clone(), strategy);
     }
 
     fn set_apy(
         env: &Env,
         to: Address,
+        memo: u64,
         amount: i128,
     ) {
         let admin = Self::get_admin(env);
 
-        let strategy = Self::get_strat(env, to.clone());
+        let strategy = Self::get_strat(env, to.clone(), memo);
         let token_client: CustomTokenClient<'_> = CustomTokenClient::new(env, &strategy.token);
         token_client.mint(&to, &amount, &admin);
     }
 
-    pub fn get_apy(env: &Env, user: Address) -> i128 {
-        let strategy = Self::get_strat(env, user.clone());
+    pub fn get_apy(env: &Env, user: Address, memo: u64) -> i128 {
+        let strategy = Self::get_strat(env, user.clone(), memo);
         let token_client = CustomTokenClient::new(env, &strategy.token);
         token_client.read_balance(&user)
     }
 
-    pub fn accrue(env: Env, user: Address, amount: i128) -> Result<(), YieldError> {
+    pub fn accrue(env: Env, user: Address, memo: u64, amount: i128) -> Result<(), YieldError> {
         let strategy_exists = env.storage().instance().has(&user);
         if !strategy_exists {
             panic_with_error!(&env, YieldError::InvalidStrategy);
         }
-        Self::set_apy(&env, user, amount);
+        Self::set_apy(&env, user, memo, amount);
         Ok(())
     }
 
-    pub fn withdraw(env: Env, amount: i128, user: Address) -> Result<(), YieldError> {
+    pub fn withdraw(env: Env, amount: i128, user: Address, memo: u64) -> Result<(), YieldError> {
         let admin = Self::get_admin(&env);
         admin.require_auth();
 
-        let strategy = Self::get_strat(&env, user.clone());
+        let strategy = Self::get_strat(&env, user.clone(), memo);
         if strategy.status != StrategyStatus::Completed {
             panic_with_error!(&env, YieldError::InvalidStatus);
         }
         let token_client: CustomTokenClient<'_> = CustomTokenClient::new(&env, &strategy.token);
         let balance = token_client.read_balance(&user);
-        if balance <= amount {
+        if balance < amount {
             panic_with_error!(&env, YieldError::NotEnoughBalance);
         }
         token_client.burn(&user, &amount, &Self::get_admin(&env));
